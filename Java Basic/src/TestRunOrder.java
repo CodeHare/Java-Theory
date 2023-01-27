917274f6fa434f28858eb2fbbe224dced472a1e7
CodeHare <sumu_qr@163.com>
2023-01-27 20:57:55 +0800

    feat: 缘起性空!

--- Java Basic/src/TestRunOrder.java
+++ Java Basic/src/TestRunOrder.java
@@ -2,6 +2,7 @@ public class TestRunOrder {
     /**/
     public static void main(String[] args) {
         System.out.println("TestRunOrder File Run!");
+        System.out.println("缘起性空");
         System.out.println("不要把人生想的那么的美好，其实都没有!");
     }
 }
